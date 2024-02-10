7bfefdb3eb78653589251392a7831d6b91c430c5
matcinks <x.mateuszstepien@gmail.com>
2024-02-10 10:31:06 +0100

    done: flatmap in collections

--- Collections/FlatMap/src/Task.kt
+++ Collections/FlatMap/src/Task.kt
@@ -1,7 +1,13 @@
 // Return all products the given customer has ordered
 fun Customer.getOrderedProducts(): List<Product> =
-        TODO()
+        orders.flatMap {
+                it.products
+        }
+//        orders.flatMap(Order::products)products
 
 // Return all products that were ordered by at least one customer
 fun Shop.getOrderedProducts(): Set<Product> =
-        TODO()
+        customers.flatMap {
+                it.getOrderedProducts()
+        }.toSet()
+//        customers.flatMap(Customer::getOrderedProducts).toSet()
