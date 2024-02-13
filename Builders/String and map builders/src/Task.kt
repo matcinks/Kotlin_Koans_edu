b716c760af4981c403bf5fb45c0d00a1ef8ec26c
matcinks <x.mateuszstepien@gmail.com>
2024-02-13 13:34:14 +0100

    done: function literals with receivers, string and map builders, the function apply

--- Builders/String and map builders/src/Task.kt
+++ Builders/String and map builders/src/Task.kt
@@ -1,6 +1,10 @@
 import java.util.HashMap
 
-/* TODO */
+fun <K, V> buildMutableMap(build: HashMap<K, V>.() -> Unit): Map<K, V>{
+    val hashMap = HashMap<K, V>()
+    hashMap.build()
+    return hashMap
+}
 
 fun usage(): Map<Int, String> {
     return buildMutableMap {
